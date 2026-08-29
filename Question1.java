dce8a40840a19a91ade10faee5e62fdd0ad63fa0
Tresford Chipili <ct23019012@biust.ac.bw>
2026-08-30 00:43:47 +0200

    Add Question 1: personal profile

---
+++ Question1.java
@@ -0,0 +1,17 @@
+// TRESFORD CHIPILI 23019012 BSc INFORMATION SYSTEMS
+// QUESTION 1 : PERSONAL PROFILE
+
+public class Question1 {
+    public static void main(String[] args) {
+        
+      String name = "Tresford Chipili";
+      int age = 22;
+      double GPA = 3.67;
+      boolean fullTimeStatus = true;
+      
+      System.out.println("My name is " + name);
+      System.out.println("I am " + age + " years old"); // int concatenated into the string //
+      System.out.println("My current gpa is " + GPA);
+      System.out.println("I am a full time student: " + fullTimeStatus);
+    }
+}
